278a100cfde233639aa2be96ccb52ceb7980b6f7
Izzatyaris <izzatybinti.aris@live.iium.edu.my>
2023-12-06 13:47:10 +0800

    Update storage.dart

--- lib/storage.dart
+++ lib/storage.dart
@@ -1,3 +1,35 @@
 // storage.dart
 //zaty
 import 'parcel.dart';
+
+class Storage {
+  int capacity = 5;
+  List<Parcel> parcels = [];
+
+  bool isFull() {
+    return parcels.length >= capacity;
+  }
+
+  void storeParcel(Parcel parcel) {
+    if (!isFull()) {
+      parcels.add(parcel);
+      print('Parcel for house ${parcel.houseNumber} stored successfully.');
+    } else {
+      print('Storage is full. Cannot store more parcels.');
+    }
+  }
+
+  Parcel collectParcel() {
+    if (parcels.isNotEmpty) {
+      return parcels.removeAt(0);
+    } else {
+      print('No parcels in storage.');
+      return Parcel('', 0); // Placeholder for no parcel
+    }
+  }
+
+  @override
+  String toString() {
+    return 'Parcels in Storage: $parcels';
+  }
+}
